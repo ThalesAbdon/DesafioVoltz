081f737d3717e37458dcd8860042b48f9042e068
Thales Sousa <thales.sousufpi@gmail.com>
2022-07-26 22:00:38 -0300

    metodo qualityAdd( adiciona qualidade para os devidos itens

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -14,7 +14,14 @@ class Shop {
 //Se a qualidade do item é maior que 0, o item perde 1 de qualidade
 qualityDrop(i) {
   if (this.items[i].quality > 0) {
-      this.items[i].quality -= 1; 
+      this.items[i].quality -= 1
+  }
+}
+
+//Se a qualidade do item é menor que 50, o item ganha 1 de qualidade
+qualityAdd(i){
+  if(this.items[i].quality < 50){
+    this.items[i].quality += 1
   }
 }
 
@@ -42,20 +49,18 @@ verifyConjured(i){
               this.qualityDrop(i)
             }
               this.qualityDrop(i)
-            
-       
       } else {
         if (this.qualityCheckUnder50(i)) {
-          this.items[i].quality = this.items[i].quality + 1;
+          this.qualityAdd(i)
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.qualityCheckUnder50(i)) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.qualityAdd(i)
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.qualityCheckUnder50(i)) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.qualityAdd(i)
               }
             }
           }
@@ -64,6 +69,7 @@ verifyConjured(i){
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+      
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -84,6 +90,8 @@ verifyConjured(i){
           }
         }
       }
+       
+
     }
 
     return this.items;
