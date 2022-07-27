2a537dec0ea63a7a3a284c15828707196b5d0c15
Thales Sousa <thales.sousufpi@gmail.com>
2022-07-26 21:54:09 -0300

    feature para itens conjurados( a qualidade diminuem 2x mais rápida)

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,13 +10,15 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
-  
+
+//Se a qualidade do item é maior que 0, o item perde 1 de qualidade
 qualityDrop(i) {
   if (this.items[i].quality > 0) {
-      this.items[i].quality = this.items[i].quality - 1; 
+      this.items[i].quality -= 1; 
   }
 }
 
+//Verifica se o item tem uma qualidade menor que 50, caso sim(true) retorna verdadeiro, caso contrário, retorna falso.
 qualityCheckUnder50(i){
   if(this.items[i].quality < 50){
    return true
@@ -24,13 +26,24 @@ qualityCheckUnder50(i){
   return false
 }
 
+//Verifica se o item é conjurado, caso seja, retorna verdadeiro(true)
+verifyConjured(i){
+  if(this.items[i].name === 'Conjured Mana Cake'){
+    return true;
+  }
+}
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && 
           this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && 
           this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-       this.qualityDrop(i)
+            if(this.verifyConjured(i)){
+              this.qualityDrop(i)
+            }
+              this.qualityDrop(i)
+            
+       
       } else {
         if (this.qualityCheckUnder50(i)) {
           this.items[i].quality = this.items[i].quality + 1;
@@ -56,7 +69,10 @@ qualityCheckUnder50(i){
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+                if(this.verifyConjured(i)){
+                  this.qualityDrop(i)
+                }
+                  this.qualityDrop(i)
               }
             }
           } else {
